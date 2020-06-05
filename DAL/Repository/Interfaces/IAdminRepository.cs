bfd69eff9579e1e6bd18dc96d59624c5ed328571
ChewbaccaLOL <apocalypseman99@gmail.com>
2020-06-05 20:20:21 +0300

    Added Contracts of Repositories

---
+++ DAL/Repository/Interfaces/IAdminRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Entities;
+using DAL.Repository.Interfaces;
+
+namespace DAL.Repository.Interfaces
+{
+    interface IAdminRepository
+        :IRepository<admins>
+    {
+    }
+}
