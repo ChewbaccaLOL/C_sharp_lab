4bb0a31465872a5f772b8d14cee6a49bb6e31d61
ChewbaccaLOL <apocalypseman99@gmail.com>
2020-06-05 23:59:06 +0300

    Added IUnitOfWork

--- DAL/Repository/Impl/BaseRepository.cs
+++ DAL/Repository/Impl/BaseRepository.cs
@@ -7,32 +7,32 @@ using DAL.EF;
 using DAL.Repository.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
-
 namespace DAL.Repository.Impl
 {
     public abstract class BaseRepository<T>
-        : IRepository<T> where T : class
+        : IRepository<T>
+        where T : class
     {
         private readonly DbSet<T> _set;
         private readonly DbContext _context;
-​
+
         public BaseRepository(DbContext context)
         {
             _context = context;
             _set = context.Set<T>();
         }
-​
+
         public void Create(T item)
         {
             _set.Add(item);
         }
-​
+
         public void Delete(int id)
         {
             var item = Get(id);
             _set.Remove(item);
         }
-​
+
         public IEnumerable<T> Find(
             Func<T, bool> predicate,
             int pageNumber = 0,
@@ -44,17 +44,17 @@ namespace DAL.Repository.Impl
                     .Take(pageNumber)
                     .ToList();
         }
-​
+
         public T Get(int id)
         {
             return _set.Find(id);
         }
-​
+
         public IEnumerable<T> GetAll()
         {
             return _set.ToList();
         }
-​
+
         public void Update(T item)
         {
             _context.Entry(item).State = EntityState.Modified;
---
+++ DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Entities;
+using DAL.EF;
+using DAL.Repository.Interfaces;
+
+
+namespace DAL.UnitOfWork
+{
+    interface IUnitOfWork: IDisposable
+    {
+        IAdminRepository adminss { get; }
+        IPostContentsRepository postContentss { get; }
+        IPostsRepository postss { get; }
+        IThumbNailsRepository thumbNailss { get; }
+        void Save();
+    }
+}
