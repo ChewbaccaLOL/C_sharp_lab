6bbef803611b38b2451771ddf135f9c4d5e15a9f
ChewbaccaLOL <apocalypseman99@gmail.com>
2020-06-06 22:43:39 +0300

    Added Identities

---
+++ CCL/Security/Identity/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CCL.Security.Identity
+{
+    public abstract class User
+    {
+        public User(int userId, string name, int postid, string userType)
+        {
+            UserId = userId;
+            Name = name;
+            POSTID = postid;
+            UserType = userType;
+        }
+        public int UserId { get; }
+        public string Name { get; }
+        public int POSTID { get; }
+        protected string UserType { get; }
+    }
+}
---
+++ CCL/Security/Identity/Visitor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CCL.Security.Identity
+{
+    public class Visitor
+        :User
+    {
+        public Visitor(int userid, string name, int postid)
+            :base(userid, name, postid, nameof(Visitor))
+        {
+
+        }
+    }
+}
