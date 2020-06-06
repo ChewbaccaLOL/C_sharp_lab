fe13556e14bb258f735c11873af5ccd3f05ec8ef
ChewbaccaLOL <apocalypseman99@gmail.com>
2020-06-06 03:17:30 +0300

    Added Delete and Get Tests

--- DAL.Tests/BaseRepositoryUnitTests.cs
+++ DAL.Tests/BaseRepositoryUnitTests.cs
@@ -49,5 +49,71 @@ namespace DAL.Tests
                     expectedPost
                     ), Times.Once());
         }
+
+        [Fact]
+        public void Get_InputId_CalledFindMethodOfDBSetWithCorrectId()
+        {
+
+            // Arrange
+            DbContextOptions opt = new DbContextOptionsBuilder<PostContext>()
+                .Options;
+            var mockContext = new Mock<PostContext>(opt);
+            var mockDbSet = new Mock<DbSet<posts>>();
+            mockContext
+                .Setup(context =>
+                    context.Set<posts>(
+                        ))
+                .Returns(mockDbSet.Object);
+
+            posts expectedStreet = new posts() { postID = 1 };
+            mockDbSet.Setup(mock => mock.Find(expectedStreet.postID))
+                    .Returns(expectedStreet);
+            var repository = new TestPostRepository(mockContext.Object);
+
+            //Act
+            var actualStreet = repository.Get(expectedStreet.postID);
+
+            // Assert
+            mockDbSet.Verify(
+                dbSet => dbSet.Find(
+                    expectedStreet.postID
+                    ), Times.Once());
+            Assert.Equal(expectedStreet, actualStreet);
+        }
+
+        [Fact]
+        public void Delete_InputId_CalledFindAndRemoveMethodsOfDBSetWithCorrectArg()
+        {
+            // Arrange
+            DbContextOptions opt = new DbContextOptionsBuilder<PostContext>()
+                .Options;
+            var mockContext = new Mock<PostContext>(opt);
+            var mockDbSet = new Mock<DbSet<posts>>();
+            mockContext
+                .Setup(context =>
+                    context.Set<posts>(
+                        ))
+                .Returns(mockDbSet.Object);
+            //EFUnitOfWork uow = new EFUnitOfWork(mockContext.Object);
+            //IStreetRepository repository = uow.Streets;
+            var repository = new TestPostRepository(mockContext.Object);
+
+            posts expectedStreet = new posts() { postID = 1 };
+            mockDbSet.Setup(mock => mock.Find(expectedStreet.postID)).Returns(expectedStreet);
+
+            //Act
+            repository.Delete(expectedStreet.postID);
+
+            // Assert
+            mockDbSet.Verify(
+                dbSet => dbSet.Find(
+                    expectedStreet.postID
+                    ), Times.Once());
+            mockDbSet.Verify(
+                dbSet => dbSet.Remove(
+                    expectedStreet
+                    ), Times.Once());
+        }
+
     }
 }
